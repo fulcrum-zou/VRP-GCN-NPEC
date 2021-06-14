406e8f54c128302211ea6ce85d52235ec29cd5f0
yutong <zouyt1110@gmail.com>
2021-06-14 18:53:29 +0800

    Update configs.py

--- code/configs.py
+++ code/configs.py
@@ -1,6 +1,15 @@
-file_path = '../dataset/'
+import torch
 
-batch_size = 20
+file_path = '../dataset/'
+file_name = 'G-20'
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+
+node_hidden_dim = 100
+edge_hidden_dim = 100
+gcn_num_layers = 3
+
+batch_size = 32
 node_num = 20          # number of customers
 initial_capacity = 40  # initial capacity of vehicles
-k_nearest = 10         # number of nearest neighbors
+k = 10         # number of nearest neighbors
