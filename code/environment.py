d2fcbf8143c6c87f625d733fe752f4ab2d60326c
yutong <zouyt1110@gmail.com>
2021-06-19 18:46:24 +0800

    Update environment

--- code/environment.py
+++ code/environment.py
@@ -3,38 +3,86 @@ from model import *
 
 class Environment:
     def __init__(self, graph, demand, distance):
+        '''
+        @param graph: (batch_size, node_num(N+1), 2)
+        @param demand: (batch_size, node_num(N+1))
+        @param distance: (batch_size, node_num(N+1), node_num(N+1))
+        '''
         self.graph = graph
         self.demand = demand
         self.distance = distance
-        self.visited = np.zeros(graph.shape[1])
         
         self.batch_size = batch_size
         self.node_num = node_num
         self.initial_capacity = initial_capacity
-        self.k_nearest = k_nearest
+        self.k = k
 
-    def load_data(self):
-        ''' load train dataset or test dataset
-        '''
+        self.visited, self.routes, self.remaining_capacity, self.remaining_demands = self.init_state()
+        self.time_step = 0
 
-    def get_state(self):
+    def init_state(self):
         '''
-        @return: customer coordinates and demand, vehicle states
+        visited: (batch_size, node_num+1)
+        routes: (batch_size, 1)
+        remaining_capacity: (batch_size, 1)
+        remaining_demands: (batch_size, node_num+1)
         '''
+        visited = torch.zeros(self.batch_size, self.node_num+1, dtype=torch.bool)
+        visited[:, 0] = True
+        routes = torch.full((self.batch_size, 1), 0)
+        remaining_capacity = torch.full(size=(self.batch_size, 1), fill_value=self.initial_capacity, dtype=torch.float)
+        remaining_demands = torch.FloatTensor(self.demand)
+        return visited, routes, remaining_capacity, remaining_demands
 
     def step(self, action):
         ''' update customer and vehicle states
-        @param action:
-        @return: reward
+        @param action: (batch_size, idx(1))
+        1. visited[idx] = True
+        2. routes += action
+        3. remaining_capacity = 
+            if idx == 0: initial_capacity
+            otherwise: max(0, remaining_capacity - demands[idx])
+        4. remaining_demands[idx] = 0
+        5. time_step += 1
         '''
+        action = action.squeeze(-1)
+        # 1.
+        self.visited[:,action] = True
+        # 2.
+        self.routes = torch.cat((self.routes, action.unsqueeze(1)), dim=1)
+        # 3.
+        prev_capacity = self.remaining_capacity
+        curr_demands = self.remaining_demands.gather(1, action.unsqueeze(1))
+        self.remaining_capacity[action==0] = self.initial_capacity
+        self.remaining_capacity[action!=0] = torch.maximum(torch.zeros(self.batch_size, 1), prev_capacity[action!=0] - curr_demands[action!=0])
+        # 4.
+        self.remaining_demands[:,action] = 0
+        # 5.
+        self.time_step += 1
 
-    def mask(self):
+    def mask(self, last_mask):
         ''' compute the mask for current states
+        @param last_mask: (batch_size, node_num+1)
+        1. if remaining_demands[idx] == 0 or
+            remaining_demands[idx] >= remaining_capacity: set idx mask True
+        2. if last_idx == 0 or t == 1: set the warehouse mask True
+        3. if mask is all True: set the warehouse mask False
         '''
+        mask = last_mask
+        # 1.
+        mask[self.remaining_demands==0] = True
+        mask[self.remaining_demands>self.remaining_capacity] = True
+        # 2.
+        if self.time_step == 1:
+            mask[:, 0] = True
+        mask[self.routes[:, -2]==0, 0] = True
+        # 3.
+        mask[mask.all(dim=1), 0] = False
+        print(mask)
+        return mask
 
-    def get_reward(self, state, action):
+    def get_reward(self, action):
         '''
-        @param state:
-        @param action:
+        @param action: (batch_size, 1)
         @return: routing distance after the action
         '''
