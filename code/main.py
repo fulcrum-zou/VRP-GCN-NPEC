7b1790a7c899dc450e69289e45c887f7688fc91f
yutong <zouyt1110@gmail.com>
2021-07-10 14:08:06 +0800

    Update code

--- code/main.py
+++ code/main.py
@@ -14,12 +14,16 @@ model = Model(node_hidden_dim, edge_hidden_dim, gcn_num_layers, k).to(device)
 criterion = CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 train_loss = []
+train_dist = []
 
 for i in range(num_epochs):
     if i == 2:
         break
     loss_per_epoch = 0
+    dist_per_epoch = 0
+    batch_num = 0
     for item in tqdm.tqdm(train_loader):
+        batch_num += 1
         graph, demand, distance = item[0].to(device), item[1].to(device), item[2].to(device)
         env = Environment(graph, demand, distance)
         sample_logprob, sample_distance, greedy_distance, target_matrix, predict_matrix = model(env)
@@ -33,13 +37,17 @@ for i in range(num_epochs):
         loss.backward()
         optimizer.step()
         loss_per_epoch += loss
+        dist_per_epoch += env.calc_distance().sum()
+        if batch_num == 3:
+            break
         
-    loss_per_epoch /= batch_size
+    loss_per_epoch /= (batch_size * batch_num)
+    dist_per_epoch /= (batch_size * batch_num)
     train_loss.append(loss_per_epoch)
-    print('epoch: %d -train loss: %.4f' %(i, train_loss[-1]))
-    f = open('../result/train_loss.txt', 'a')
-    f.close()
+    train_dist.append(dist_per_epoch)
+    print('epoch: %d -train loss: %.2f - distance: %.2f' %(i, train_loss[-1], train_dist[-1]))
     write_loss('train_loss.txt', i, train_loss[-1])
-
-# torch.save(model.state_dict(), '../result/params.pkl')
-plot_loss(train_loss)
+    write_distance('train_dist.txt', i, train_dist[-1])
+    # torch.save(model.state_dict(), '../result/params.pkl')
+    plot_loss(train_loss)
+    plot_dist(train_dist)
--- code/utils.py
+++ code/utils.py
@@ -7,25 +7,42 @@ def write_loss(file_name, epoch, loss):
     file_path = '../result/' + file_name
     mode = 'w' if epoch == 0 else 'a'
     f = open(file_path, mode)
-    f.write('%d %.4f\n' %(epoch, loss))
+    f.write('%d %.2f\n' %(epoch, loss))
+    f.close()
+
+def write_distance(file_name, epoch, dist):
+    file_path = '../result/' + file_name
+    mode = 'w' if epoch == 0 else 'a'
+    f = open(file_path, mode)
+    f.write('%d %.2f\n' %(epoch, dist))
     f.close()
 
 def plot_loss(loss):
+    plt.cla()
     file_path = '../result/' + 'loss.png'
     plt.plot(loss, color='skyblue', linewidth=1)
     plt.title('Train Loss')
     plt.xlabel('epochs')
     plt.ylabel('loss')
     plt.savefig(file_path)
+    
+def plot_dist(dist):
+    plt.cla()
+    file_path = '../result/' + 'dist.png'
+    plt.plot(dist, color='skyblue', linewidth=1)
+    plt.title('Train Distance')
+    plt.xlabel('epochs')
+    plt.ylabel('distance')
+    plt.savefig(file_path)
 
 def write_file(file_name, train_result, test_result):
     file_path = '../result/' + file_name
     f = open(file_path, 'a')
     for i in range(len(train_result)):
-        f.write('%.4f ' %train_result[i][0])
-        f.write('%.4f\n' %train_result[i][1])
-        f.write('%.4f ' %test_result[i][0])
-        f.write('%.4f\n' %test_result[i][1])
+        f.write('%.2f ' %train_result[i][0])
+        f.write('%.2f\n' %train_result[i][1])
+        f.write('%.2f ' %test_result[i][0])
+        f.write('%.2f\n' %test_result[i][1])
     f.close()
 
 def read_file(file_name):
